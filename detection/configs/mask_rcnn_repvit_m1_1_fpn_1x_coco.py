cac7765e7c2364599e081353f467c3e546686773
ben <burbancic3@gmail.com>
2025-05-07 05:30:28 +0000

    update rep

--- detection/configs/mask_rcnn_repvit_m1_1_fpn_1x_coco.py
+++ detection/configs/mask_rcnn_repvit_m1_1_fpn_1x_coco.py
@@ -1,7 +1,7 @@
 _base_ = [
     "mmdet::_base_/models/mask-rcnn_r50_fpn.py",
-    "_base_/datasets/coco_instance.py",
-    "_base_/schedules/schedule_1x.py",
+    "mmdet::_base_/datasets/coco_instance.py",
+    "mmdet::_base_/schedules/schedule_1x.py",
     "mmdet::_base_/default_runtime.py",
 ]
 # optimizer
--- detection/trainy.py
+++ detection/trainy.py
@@ -9,6 +9,8 @@ from mmengine.runner import Runner
 
 from mmdet.utils import setup_cache_size_limit_of_dynamo
 
+import repvit
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
