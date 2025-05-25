23a09aae851a39a7abd59f8931176e8b00a6343c
ben <burbancic3@gmail.com>
2025-05-25 00:40:13 +0000

    add new config

---
+++ detection/configs/faster_rcnn/faster-rcnn_r50_fpn_1x_coco_person_ben.py
@@ -0,0 +1,27 @@
+_base_ = [
+    "../_base_/models/faster-rcnn_r50_fpn_ben.py",
+    "../_base_/datasets/coco_detection_ben.py",
+    "../_base_/schedules/schedule_1x_ben.py",
+    "../_base_/default_runtime_ben.py",
+]
+classes = [
+    "person",
+]
+model = dict(
+    backbone=dict(
+        type="repvit_m1_1",
+        init_cfg=dict(
+            type="Pretrained",
+            checkpoint="pretrain/repvit_m1_1_distill_300e.pth",
+        ),
+        out_indices=[2, 6, 20, 24],
+    ),
+    neck=dict(
+        type="FPN", in_channels=[64, 128, 256, 512], out_channels=256, num_outs=5
+    ),
+    roi_head=dict(
+        bbox_head=dict(
+            num_classes=1,
+        )
+    )
+)
